08305562f18e2ab3f176dff1c1fc70bcd6d1a162
Sahil Mehta <smehta3303@users.noreply.github.com>
2023-01-18 11:59:11 -0500

    Cleanup

--- TapDancer.ino
+++ TapDancer.ino
@@ -5,20 +5,10 @@
 // constants
 static constexpr uint16_t DebounceTimerInterval = 10 * 1000;    // 10 ms
 static constexpr uint8_t LoopPeriod = 100;      // 100 ms
-
-uint32_t time_since_last_run = 0;
+elapsedMillis run_interval = 0;
 
 Hardware hw;
 IntervalTimer debouncer;
-volatile bool toggle = false;
-bool previous_toggle_val = toggle;
-
-uint8_t previous_pin_state = 1;
-uint8_t debounce_count = 0;
-
-SwitchState previous_switch_state = SwitchState::UNINITIALIZED;
-SwitchState switch_state = SwitchState::UNINITIALIZED;
-
 
 void setup() {
     Serial.begin(115200);
@@ -30,35 +20,23 @@ void setup() {
 
 }
 
-void scheduler_callback() {
-    if (toggle == false) {
-        toggle = true;
-    } else {
-        toggle = false;
-    }
-}
-
 void debounce_callback() {
     hw.GetSwitch1().Debounce();
 }
 
 void process_events() {
-    switch_state = hw.GetSwitch1().GetState();
-    
-    if (previous_switch_state != switch_state) {
-        previous_switch_state = switch_state;
-        if(switch_state == SwitchState::PRESSED) {
-            Serial.println("PRESSED");
-        } else if(switch_state == SwitchState::RELEASED) {
-            Serial.println("RELEASED");
-        }
+    SwitchState switch_state = hw.GetSwitch1().GetState();
+
+    if(switch_state == SwitchState::PRESSED) {
+        hw.GetLed1().On();
+    } else {
+        hw.GetLed1().Off();
     }
 }
 
 void loop() {
-    uint32_t current_ticks_ms = millis();
-    if((current_ticks_ms - time_since_last_run) >= LoopPeriod) {
-        time_since_last_run = current_ticks_ms;
+    if(run_interval >= LoopPeriod) {
+        run_interval = 0;
 
         process_events();
         
