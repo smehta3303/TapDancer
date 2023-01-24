ce3a2eae8bfb91073baf57d07383e06b1666e7d2
Sahil Mehta <smehta3303@users.noreply.github.com>
2023-01-23 20:03:25 -0500

    Add switch events and only trigger on rising edge (#10)

--- TapDancer.ino
+++ TapDancer.ino
@@ -4,19 +4,22 @@
 // constants
 static constexpr uint16_t DebounceTimerInterval = 10 * 1000;    // 10 ms
 static constexpr uint8_t LedUpdateInterval = 20;
-static constexpr uint8_t LoopPeriod = 100;      // 100 ms
+static constexpr uint8_t LoopPeriod = 50;      // 100 ms
 
 elapsedMillis led_interval = 0;
 elapsedMillis run_interval = 0;
 
 Hardware hw;
 IntervalTimer debouncer;
-bool led_pulse = false;
+LedState led_state = LedState::LED_OFF;
 
 uint8_t pulse_count = 0;
-uint8_t max_pulse_count = 0;
+uint8_t max_pulse_count = 2 * (255 / hw.GetLed1().GetPulseSpeed());
 bool leds_toggled = false;
 bool initialized = false;
+
+SwitchState previous_switch_state = SwitchState::SW_UNINITIALIZED;
+uint32_t switch_pressed_ticks = 0;
 
 void init_sequence() {
     if (initialized) {
@@ -50,8 +53,6 @@ void init_sequence() {
         leds_toggled = true;
     }
     
-    max_pulse_count = 2 * (255 / hw.GetLed1().GetPulseSpeed());
-
     if (pulse_count != max_pulse_count) {
         pulse_count++;
         hw.GetLed1().Pulse();
@@ -93,11 +94,8 @@ void loop() {
 
     if (led_interval >= LedUpdateInterval) {
         led_interval = 0;
-        if (led_pulse) {
-            hw.GetLed1().Pulse();
-        } else {
-            hw.GetLed1().Off();
-        }
+
+        update_leds();
     }
     
     if(run_interval >= LoopPeriod) {
@@ -108,13 +106,85 @@ void loop() {
 }
 
 void process_events() {
-    SwitchState switch_state = hw.GetSwitch1().GetState();
 
-    if(switch_state == SwitchState::PRESSED) {
-        if (!led_pulse) {
-            led_pulse = true;
-        } else {
-            led_pulse = false;
+    SwitchEvent event = get_switch_events();
+    LedState current_state = hw.GetLed1().GetState();
+    
+    switch (event) {
+        case SwitchEvent::FS1_PRESSED:
+            Serial.println("Got Pressed Event");
+            if(current_state == LedState::LED_OFF) {
+                led_state = LedState::LED_ON;
+                Serial.println("FS1_PRESSED: led_state: On");
+                
+            } else {
+                led_state = LedState::LED_OFF;
+                Serial.println("FS1_PRESSED: led_state: Off");
+            }
+            break;
+
+        case SwitchEvent::FS1_CONFIGURE:
+            if(current_state == LedState::LED_OFF) {
+                led_state = LedState::LED_PULSE;
+                Serial.println("FS1_CONFIGURE: led_state: Pulse");
+            } else {
+                led_state = LedState::LED_OFF;
+                Serial.println("FS1_CONFIGURE: led_state: Off");
+            }
+            break;
+        case SwitchEvent::FS1_RELEASED:
+            led_state = LedState::LED_OFF;
+            Serial.println("FS1_RELEASED: led_state: Off");
+            break;
+
+        case SwitchEvent::NO_EVENT:
+            return;
+        default:
+            led_state = LedState::LED_OFF;
+            Serial.println("default: led_state: Off");
+    }
+}
+
+SwitchEvent get_switch_events() {
+    SwitchState switch_state = hw.GetSwitch1().GetState();
+    SwitchEvent event = SwitchEvent::NO_EVENT;
+
+    if (previous_switch_state != switch_state) {
+        Serial.println("GSE: Inside first if");
+
+        if(switch_state == SwitchState::SW_PRESSED) {
+            switch_pressed_ticks = millis();
+            Serial.println("GSE: SW_PRESSED: Starting timer");
+        } else if (switch_state == SwitchState::SW_RELEASED) {
+            if ((millis() - switch_pressed_ticks) >= 3000) {
+                event = SwitchEvent::FS1_CONFIGURE;
+                Serial.println("GSE: Event: FS1_CONFIGURE");
+            } else if (previous_switch_state == SwitchState::SW_PRESSED) {
+                event = SwitchEvent::FS1_PRESSED;
+                Serial.println("GSE: Event: FS1_PRESSED");
+            } else {
+                event = SwitchEvent::FS1_RELEASED;
+                Serial.println("GSE: Event: FS1_RELEASED");
+            }
         }
-    } 
+        previous_switch_state = switch_state;
+    }
+    return event;
+}
+
+void update_leds() {
+    switch(led_state) {
+        case LedState::LED_ON:
+             hw.GetLed1().On();
+             break;
+
+        case LedState::LED_PULSE:
+            hw.GetLed1().Pulse();
+            break;
+
+        case LedState::LED_OFF:
+        default:
+            hw.GetLed1().Off();
+            break;
+    }
 }
--- src/led.cpp
+++ src/led.cpp
@@ -11,15 +11,23 @@ void Led::Initialize() const {
 
 void Led::On() const {
     pin_.On(255);
+    state_ = LedState::LED_ON;
 }
 
 void Led::Off() const {
    pin_.On(0);
    duty_cycle_ = 0;
+   state_ = LedState::LED_OFF;
 }
 
 void Led::Toggle() const {
     pin_.Toggle();
+    uint8_t pin_state = pin_.Read();
+    if(pin_state == HIGH) {
+        state_ = LedState::LED_ON;
+    } else {
+        state_ = LedState::LED_OFF;
+    }
 }
 
 void Led::Pulse() const {
@@ -35,4 +43,5 @@ void Led::Pulse() const {
         duty_cycle_ -= PulseSpeed;
     }
     pin_.On(duty_cycle_);
+    state_ = LedState::LED_PULSE;
 }
